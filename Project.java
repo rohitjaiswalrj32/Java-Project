ae832dd574911c36fcacea9b991b7a077f295a37
Manish-Pandey-ji <143824608+Manish-Pandey-ji@users.noreply.github.com>
2024-09-25 16:39:52 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,4 +1,18 @@
-public class Person implements Cloneable {
+
+package Array;
+
+public class simple { 
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Person originalPerson = new Person("Alice", 30);
+        Person clonedPerson = (Person) originalPerson.clone();
+
+        System.out.println("Original Person: " + originalPerson);
+        System.out.println("Cloned Person: " + clonedPerson);
+    }
+}
+
+class Person implements Cloneable {
     private String name;
     private int age;
 
@@ -7,26 +21,14 @@ public class Person implements Cloneable {
         this.age = age;
     }
 
-    // Override the clone() method
+    
     @Override
     protected Object clone() throws CloneNotSupportedException {
-        // Create a shallow copy using super.clone()
+        
         Person clone = (Person) super.clone();
 
-        // If necessary, modify the cloned object's fields for deep copying
-        // For example, if the object contains mutable references, you might need
-        // to clone those references as well to avoid sharing them with the original
+        
 
         return clone;
     }
-
-    // Getters and setters
-    // ...
 }
-
-// Usage:
-Person originalPerson = new Person("Alice", 30);
-Person clonedPerson = (Person) originalPerson.clone();
-
-System.out.println("Original Person: " + originalPerson);
-System.out.println("Cloned Person: " + clonedPerson);
