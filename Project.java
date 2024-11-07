54113bb107c73ed8c79b9bbedd7dc93c658d3af9
Rohit Jaiswal <112413070+rohitjaiswalrj32@users.noreply.github.com>
2024-11-07 13:27:50 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,34 +1,11 @@
+public class SumOfTwoNumbers {
 
-package Array;
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
 
-public class Project { 
+        int sum = num1 + num2;
 
-    public static void main(String[] args) throws CloneNotSupportedException {
-        Person originalPerson = new Person("Alice", 30);
-        Person clonedPerson = (Person) originalPerson.clone();
-
-        System.out.println("Original Person: " + originalPerson);
-        System.out.println("Cloned Person: " + clonedPerson);
-    }
-}
-
-class Person implements Cloneable {
-    private String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        
-        Person clone = (Person) super.clone();
-
-        
-
-        return clone;
+        System.out.println("The sum is: " + sum);
     }
 }
