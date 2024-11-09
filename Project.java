6eed59c59af3c401e4475d42774a468c814aac72
Rohit Jaiswal <112413070+rohitjaiswalrj32@users.noreply.github.com>
2024-11-09 12:30:46 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,11 +1,5 @@
-public class SumOfTwoNumbers {
-
-    public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-
-        int sum = num1 + num2;
-
-        System.out.println("The sum is: " + sum);
-    }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
